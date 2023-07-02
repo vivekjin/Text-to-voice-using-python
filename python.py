a5bcfda27e5ce826ded370b1f61500f42926e24b
vivek jin <47064256+vivekjin@users.noreply.github.com>
2023-07-02 19:43:26 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+import os
+if __name__ == '__main__':
+    print("welcome tp RoboSpeaker by vivek")
+    while True:
+        x = input("enter command \n")
+        if x == "":
+            break
+        command = f'mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""{x}"")(window.close)")'
+        os.system(command)
